bf8b0498fd82a7dc0a424fd2c389d59f3a7597da
Shawn Shen <senhtry@msn.com>
2022-12-06 09:29:41 +0800

    Add project files.

---
+++ WaterSteamProperty/UnitConversion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WaterSteamProperty
+{
+    public partial class StmProp
+    {
+        /* Unit conversion routines
+         * 
+         * Convert from SI to BTU if unit = 1
+         * Convert from BTU to SI if unit = -1
+         * 
+         */
+
+        double ConvertT(double tin, int unit)
+        {
+            return tin;
+        }
+    }
+}
